e882f39b47da6bb4317f5ef40148445aea431eec
Lisj <549856098@qq.com>
2017-02-28 17:29:57 +0800

    网易阅读

---
+++ app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="a702b0fc-838f-4e70-b321-fc7706081246", modulePackage="com.example.jingbin.cloudreader", sdkRoot="D:\\androidSDK\\android-sdk-windows",layoutInfoDir="E:\\AndroidStudioProjects\\page\\CloudReader\\app\\build\\intermediates\\data-binding-info\\debug",exportClassListTo="",isLibrary=false,minSdk=17,enableDebugLogs=false,printEncodedError=true)
+public class DataBindingInfo {}
